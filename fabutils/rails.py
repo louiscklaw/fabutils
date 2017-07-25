8d7d40eaff158f42060b8a505c98e7b8e8a2704e
RajaRaviVarma <ravi.varma@cybrilla.com>
2017-07-25 13:29:01 +0530

    Add db_migrate to rails module
    * This is to run `rake db:migrate`.

--- fabutils/rails.py
+++ fabutils/rails.py
@@ -8,3 +8,12 @@ def bundle_install(args=''):
     args -- Any additional options to be passed to the bundle command
     """
     sudo('bundle install {}'.format(args))
+
+def db_migrate(args=''):
+    """
+    Execute ./bin/rake db:migrate to run migrations
+
+    Arguments:
+    args -- Any additional options to be passed to the rake command
+    """
+    sudo('./bin/rake db:migrate {}'.format(args))
