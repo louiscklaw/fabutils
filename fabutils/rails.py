485a41e1d9211748b2fb3ddd89287e44a85c9958
RajaRaviVarma <ravi.varma@cybrilla.com>
2017-07-26 20:52:42 +0530

    Change case for argument description

--- fabutils/rails.py
+++ fabutils/rails.py
@@ -5,7 +5,7 @@ def bundle_install(args=''):
     Execute bundle install command
 
     Arguments:
-    args -- Any additional options to be passed to the bundle command
+    args -- any additional options to be passed to the bundle command
     """
     sudo('bundle install {}'.format(args))
 
